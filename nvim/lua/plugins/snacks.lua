1e69bc4209be44b140a32019940517ce7264e002
Violeta Malinova <violeta@mlnova.com>
2025-10-08 19:44:50 +0300

    Adjust the color of the directories in Snacks sidebar to be the same color as the files.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,7 @@
 return {
   {
     "folke/snacks.nvim",
+
     opts = {
       explorer = {
         hidden = true, -- show dotfiles
@@ -15,7 +16,37 @@ return {
           files = { hidden = true, ignored = true },
           explorer = { hidden = true, ignored = true },
         },
+
+        exclude = { -- add folder names here to exclude
+          ".git",
+          "node_modules",
+        },
       },
     },
+
+    config = function(_, opts)
+      require("snacks").setup(opts)
+
+      local function set_snacks_dir_color()
+        -- Make directories look like files
+        vim.api.nvim_set_hl(0, "SnacksPickerDirectory", { link = "SnacksPickerFile", nocombine = true })
+      end
+
+      -- Apply immediately
+      set_snacks_dir_color()
+
+      -- Re-apply when the colorscheme changes
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        group = vim.api.nvim_create_augroup("MySnacksDirColor", { clear = true }),
+        callback = set_snacks_dir_color,
+      })
+
+      -- Optional: re-apply when Snacks reloads (some builds emit this User event)
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "SnacksHighlights",
+        group = "MySnacksDirColor",
+        callback = set_snacks_dir_color,
+      })
+    end,
   },
 }
