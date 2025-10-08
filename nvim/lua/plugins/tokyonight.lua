e10e1e8deb988799ec205a1c1cf193d620faa805
Violeta Malinova <violeta@mlnova.com>
2025-10-08 19:26:39 +0300

    Adjust tokyonight theme to make the SnacksPicker sidebar transparent.

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -4,6 +4,9 @@ return {
     opts = {
       style = "night",
       transparent = true,
+      on_highlights = function(hl)
+        hl.SnacksPicker = { bg = "none", nocombine = true }
+      end,
     },
   },
 }
