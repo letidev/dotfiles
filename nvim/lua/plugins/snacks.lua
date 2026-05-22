b84ef453ce1437eb4ae849b8f3df53a4cd3a616b
Violeta Malinova <violeta@mlnova.com>
2026-05-22 09:11:26 +0300

    Configure snacks explorer to show hidden and ignored files by default and configure the size of the lazygit modal.
    Fix snacks explorer settings

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true, -- show dotfiles by default (toggle with shift+h)
+          ignored = true, -- show git-ignored files by default (toggle with shift+i)
+        },
+      },
+    },
+    styles = {
+      lazygit = {
+        width = 0, -- 0 = full width
+        height = 0, -- 0 = full height
+      },
+    },
+  },
+}
